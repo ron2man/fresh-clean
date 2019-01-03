2728f3bf98781d172f4389a3ea9105a5b5a20f3e
Ron Shtaiman <ron2man10@gmail.com>
2019-01-03 12:24:28 +0200

    m

--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -7,7 +7,8 @@ class Homepage extends React.Component {
     render() {
         return <div className="boxes">
             <FirstBox  />
-            <SecondBox date="1545894000000"/> {/* 27/12/2018  */}
+            <SecondBox date="1545894000000"/>
+             {/* 27/12/2018  */}
             <ThirdBox />
         </div>;
     }
